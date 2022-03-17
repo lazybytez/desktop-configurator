b5fa792a7d27dd0777e87ae6932db232f58803ec
Pascal Zarrad <p.zarrad@outlook.de>
2022-03-17 20:34:29 +0100

    feat: define input sources

--- files/load_dconf.sh
+++ files/load_dconf.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
+sleep 15
 dconf load / < ~/.lazybytez-theme/gnome-settings.ini
+busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
+sleep 15
